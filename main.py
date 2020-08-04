7284d8cdc1e3d23917e9ca7d88604d8027a303d3
marcmuon <marc.kelechava@gmail.com>
2020-08-04 16:22:41 -0700

    add np slicing to create random holdout w respect to folds

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from pathlib import Path
 from audio import AudioFeature
+from model import Model
+from sklearn.ensemble import RandomForestClassifier
+import os
+import pickle
+import numpy as np
+
 
 def parse_metadata(path):
     meta_df = pd.read_csv(path)
@@ -9,17 +15,62 @@ def parse_metadata(path):
 
     return meta
 
+
+def slice_holdout_idx(fold_arr):
+    random_idx = []
+    for i in range(1, 11):
+        random_idx.extend(
+            np.random.choice(np.where(fold_arr == i)[0], 45, replace=False).tolist()  
+        )
+    return np.array(random_idx)
+
 if __name__ == "__main__":
+
     metadata = parse_metadata("metadata/UrbanSound8K.csv")
-    
+
     audio_features = []
     for row in metadata:
+
         path, fold, label = row
-        src_path = f"{Path.home()}/datasets/UrbanSound8K/audio/fold{fold}/{path}"
-        # TODO - add check; if on disk, load, if not, extract
-        audio = AudioFeature(src_path, fold, label)
-        audio.extract_features("mfcc", "spectral", "chroma")
-        audio_features.append(audio)
 
+        fn = path.replace(".wav", "")
+        transformed_path = f"{Path.home()}/projects/urban_sound_classification/data/fold{fold}/{fn}.pkl"
 
+        if os.path.isfile(transformed_path):
+            # if the file exists as a .pkl already, then load it
+            with open(transformed_path, "rb") as f:
+                audio = pickle.load(f)
+                audio_features.append(audio)
+        else:
+            # if the file doesn't exist, then extract its features from the source data and save the result
+            src_path = f"{Path.home()}/datasets/UrbanSound8K/audio/fold{fold}/{path}"
+            audio = AudioFeature(src_path, fold, label)
+            audio.extract_features("mfcc", "spectral", "chroma")
+            audio_features.append(audio)
 
+    feature_matrix = np.vstack([audio.features for audio in audio_features])
+    labels = np.array([audio.label for audio in audio_features])
+    folds = np.array([audio.fold for audio in audio_features])
+
+    holdout_idx = slice_holdout_idx(folds)
+    
+    features_test = feature_matrix[holdout_idx, :]
+    labels_test = labels[holdout_idx]
+
+    features_train = np.delete(feature_matrix, holdout_idx, axis=0)
+    labels_train = np.delete(labels, holdout_idx, axis=0)
+    folds_train = np.delete(folds, holdout_idx, axis=0)
+
+    model_cfg = dict(
+        model=RandomForestClassifier(
+            random_state=42,
+            n_jobs=10,
+            class_weight="balanced",
+            n_estimators=500,
+            bootstrap=True,
+        ),
+    )
+    model = Model(features_train, labels_train, folds_train, model_cfg)
+    fold_acc = model.train_kfold()
+
+    # TODO - show box plot or std and mean like they were saying
--- model.py
+++ model.py
@@ -35,43 +35,5 @@ class Model:
             
             fold_acc = accuracy_score(y_test, y_pred)
             self.val_fold_scores_.append(fold_acc)
-            print("logo fold complete")
-            
-            """
-            pipe = Pipeline(
-                [("scaler", StandardScaler(copy=True)), ("model", self.cfg["model"])]
-            )
-
-            kf = StratifiedKFold(n_splits=3, random_state=42, shuffle=True)
-            
-            # TODO - This isn't valid. In this inner CV things things from same recording fold
-            # could end up in train/test of the CV. What you could do is see if you could pass another
-            # group-based splitter into GridSearchCV. It wouldn't be LOGO but rather GroupKFold
-            # and you'd have to increase the inner CV splits. Alternatively, you'd simply not do a GridSearch.
-            # That's maybe fine for the purposes here. Could still write a blog showing the pitfalls to avoid.
-            # If you did try no GridSearch, the val set is really the mean score on the 10, and you're fitting to that,
-            # if you choose between different model families, say. But then you'd want an external test set of random
-            # example that you'd need to splice out. But maybe not even worth trying to max the RF. Could instead
-            # spend the time seeing if Keras works on the non-GCV approach. I think I would just do a blog on what you have
-            # here, and discuss the pitfalls, and then do another blog later trying to beat it. Compare it to the research paper.
-
-            grid_search = GridSearchCV(
-                estimator=pipe,
-                param_grid=self.cfg["param_grid"],
-                cv=kf,
-                return_train_score=True,
-                verbose=3,
-                **self.cfg["grid_dict"]
-            )
-
-            grid_search.fit(X_train, y_train)
-            self.trained_models_.append(grid_search.best_estimator_)
-
-            X_test = grid_search.best_estimator_["scaler"].transform(X_test)
-            y_pred = grid_search.best_estimator_["model"].predict(X_test)
-
-            fold_acc = accuracy_score(y_test, y_pred)
-            self.val_fold_scores_.append(fold_acc)
-            """
 
         return self.val_fold_scores_
